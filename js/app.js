e11ec3c48ce1388a5d048b3b5ae1d19fba954c0f
mrloulass <louislassegue@gmail.com>
2021-01-21 22:06:39 -0800

    add 7th question to JS file

--- js/app.js
+++ js/app.js
@@ -5,15 +5,18 @@
 // var banana = 42
 
 // get user name and offer greeting
+
 let correctCount = 0;
+
 let userName = prompt('Hello, what\'s your name?');
+
 alert(`Hello, ${userName}.`);
 // console.log('Hello, ' + userName + '.');
 
 //write 5 questions. they must accept yes or no or y or n in any case
 //examples: YES, yes, YEs
 
-function askOne(userName) {
+function askOne() {
   let askOne = prompt('Did I lived in New Jersey?').toLowerCase();
   if (askOne === 'yes' || askOne === 'y') {
     alert(`You are right, ${userName}.`);
@@ -27,9 +30,9 @@ function askOne(userName) {
     // console.log(userName + ' ,don\'t you hear my Jersey accent?');
   }
 }
-askOne(userName);
+askOne();
 
-function askTwo(userName) {
+function askTwo() {
   let askTwo = prompt('Did I travel to France?').toLowerCase();
 
   if (askTwo === 'no' || askTwo === 'n') {
@@ -41,9 +44,9 @@ function askTwo(userName) {
     // console.log('Wrong, but maybe in the future, ' + userName + '.');
   }
 }
-askTwo(userName);
+askTwo();
 
-function askThree(userName) {
+function askThree() {
   let askThree = prompt('Do I like food from St.Louis?').toLowerCase();
 
   if (askThree === 'no' || askThree === 'n') {
@@ -55,9 +58,9 @@ function askThree(userName) {
     // console.log('Nope, I never been there before, ' + userName + '.');
   }
 }
-askThree(userName);
+askThree();
 
-function askFour(userName) {
+function askFour() {
   let askFour = prompt('Do I currently work for Lowes?').toLowerCase();
 
   if (askFour === 'no' || askFour === 'n') {
@@ -69,9 +72,9 @@ function askFour(userName) {
     // console.log('Sorry, I work for Home Depot, ' + userName + '.');
   }
 }
-askFour(userName);
+askFour();
 
-function askFive(userName) {
+function askFive() {
   let askFive = prompt('Did I learn to code from Code Fellows in Seatle?').toLowerCase();
 
   if (askFive === 'yes' || askFive === 'y') {
@@ -83,43 +86,56 @@ function askFive(userName) {
     // console.log('Wrong, ' + userName + '.');
   }
 }
-askFive(userName);
+askFive();
 
-function askSix(userName) {
+function askSix() {
   let askSix = +prompt(`${userName}, how many trips am I planning next year?`);
 
   let tryFour = 4;
 
   let trips = 8;
 
-  for (var i = 0; i < tryFour; i++) {
+  for (let i = 0; i < tryFour; i++) {
     if (askSix === trips) {
       alert('You are correct, plan a trip with me.');
+      // console.log('You are correct, plan a trip with me.');
       correctCount++;
       break;
     } else if (askSix > trips) {
       alert('Too High');
+      // console.log('Too High');
       break;
     } else if (askSix < trips) {
       alert('Too Low');
+      // console.log('Too Low');
       break;
     }
   }
 }
-askSix(userName);
+askSix();
 
-alert(`Thank you, ${userName} for visting my website. You score ${correctCount} correctly.`);
+function askSeven() {
+  let favoriteColors = ['red', 'blue', 'black', 'yellow', 'green', 'pink', 'purple'];
+  let guessSix = 6;
+  let guessCount = 0;
+  let correctColors = false;
+  let askSeven = prompt('What is my favorite colors to wear?').toLowerCase();
+  while (guessCount < guessSix) {
+    guessCount++;
+  } for (let i = 0; i < favoriteColors.length; i++) {
+    if (askSeven === favoriteColors[i]) {
+      alert(`Correct ${userName}, you see how good I look in that color.`);
+      correctCount++;
+      break;
+    } else if (askSeven !== favoriteColors.length) {
+      alert(`Sorry ${userName}, I don't look good in that color. `);
+      break;
+    } else (!correctColors);
+    alert(`${favoriteColors} are my favorite colors to wear.`);
+    break;
+  }
+}
+askSeven();
+
+alert(`Thank you, ${userName} for visting my website.You score ${correctCount} correctly.`);
 // console.log('Thank you, ' + userName + ' for visting my website. You score'+ correctCount + correctly.');
-
-
-// var favoriteColor = ['red','blue', 'green', 'yellow','orange','pink','purple'];
-
-// var guessSix = 6;
-
-// var askSeven = prompt('What is my favorite color').toLowerCase();
-
-// while(){
-
-// };
-
-
