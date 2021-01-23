a5d2108ac5354a8b7a02da45043a3d3b85da960a
mrloulass <louislassegue@gmail.com>
2021-01-23 15:42:27 -0800

    remove consol.log comments in JS

--- js/app.js
+++ js/app.js
@@ -1,33 +1,20 @@
 'use strict';
 
-//linter test working
-
-// var banana = 42
-
-// get user name and offer greeting
-
 let correctCount = 0;
 
 let userName = prompt('Hello, what\'s your name?');
 
 alert(`Hello, ${userName}.`);
-// console.log('Hello, ' + userName + '.');
-
-//write 5 questions. they must accept yes or no or y or n in any case
-//examples: YES, yes, YEs
 
 function askOne() {
   let askOne = prompt('Did I lived in New Jersey?').toLowerCase();
   if (askOne === 'yes' || askOne === 'y') {
     alert(`You are right, ${userName}.`);
     correctCount++;
-    // console.log('You are right, ' + userName + '.');
   } else if (askOne === 'no' || askOne === 'n') {
     alert(`Wrong, ${userName}.`);
-    // console.log('Wrong, ' + userName + '.');
   } else {
     askOne = alert(`${userName}, don't you hear my Jersey accent?`);
-    // console.log(userName + ' ,don\'t you hear my Jersey accent?');
   }
 }
 askOne();
@@ -38,10 +25,8 @@ function askTwo() {
   if (askTwo === 'no' || askTwo === 'n') {
     alert(`Correct ${userName}.`);
     correctCount++;
-    // console.log('Correct ' + userName + '.');
   } else if (askTwo === 'yes' || askTwo === 'y') {
     alert(`Wrong, but maybe in the future, ${userName}.`);
-    // console.log('Wrong, but maybe in the future, ' + userName + '.');
   }
 }
 askTwo();
@@ -52,10 +37,8 @@ function askThree() {
   if (askThree === 'no' || askThree === 'n') {
     alert(`Correct,${userName}.`);
     correctCount++;
-    // console.log('Correct ' + userName + '.');
   } else if (askThree === 'yes' || askThree === 'y') {
     alert(`Nope, I never been there before, ${userName}.`);
-    // console.log('Nope, I never been there before, ' + userName + '.');
   }
 }
 askThree();
@@ -66,10 +49,8 @@ function askFour() {
   if (askFour === 'no' || askFour === 'n') {
     alert(`Correct, ${userName}.`);
     correctCount++;
-    // console.log('Correct ' + userName + '.');
   } else if (askFour === 'yes' || askFour === 'y') {
     alert(`Sorry, I work for Home Depot, ${userName}.`);
-    // console.log('Sorry, I work for Home Depot, ' + userName + '.');
   }
 }
 askFour();
@@ -80,10 +61,8 @@ function askFive() {
   if (askFive === 'yes' || askFive === 'y') {
     alert(`You are right, ${userName}.`);
     correctCount++;
-    // console.log('You are right, ' + userName + '.');
   } else if (askFive === 'no' || askFive === 'n') {
     alert(`Wrong, ${userName}.`);
-    // console.log('Wrong, ' + userName + '.');
   }
 }
 askFive();
@@ -98,16 +77,13 @@ function askSix() {
   for (let i = 0; i < tryFour; i++) {
     if (askSix === trips) {
       alert('You are correct, plan a trip with me.');
-      // console.log('You are correct, plan a trip with me.');
       correctCount++;
       break;
     } else if (askSix > trips) {
       alert('Too High');
-      // console.log('Too High');
       break;
     } else if (askSix < trips) {
       alert('Too Low');
-      // console.log('Too Low');
       break;
     }
   }
@@ -138,4 +114,4 @@ function askSeven() {
 askSeven();
 
 alert(`Thank you, ${userName} for visting my website.You score ${correctCount} correctly.`);
-// console.log('Thank you, ' + userName + ' for visting my website. You score'+ correctCount + correctly.');
+
